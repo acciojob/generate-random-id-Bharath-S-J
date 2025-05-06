b0717372abd3b6fa04793bf2e7ca0349c11c7832
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-06 21:00:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
   // write your code here
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+//const l = prompt("Enter a number.");
 alert(makeid(l));
